833ade57e01072e6a531dd7c997a64702ec87bc5
ishrar776 <89441607+ishrar776@users.noreply.github.com>
2023-07-27 10:57:22 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-wvnkkc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-7te3z3)
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import './style.css';
+export default function App() {
+  const [count, setCount] = useState(0);
+  const addToCart1 = () => {
+    setCount(count + 1);
+  };
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>
+        <button onClick={addToCart1}>AddToCart</button>
+      </p>
+      {!count == 0 && <p>your count is {count}</p>}
+    </div>
+  );
+}
